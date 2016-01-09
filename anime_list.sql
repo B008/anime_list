c95b6faeb3801f9032bd2c10850c97e9856fd03b
B008 <tabi8023@yahoo.co.jp>
2016-01-09 18:42:23 +0900

    sql fg var.2

--- anime_list.sql
+++ anime_list.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016 年 1 朁E09 日 09:39
+-- Generation Time: 2016 年 1 朁E09 日 10:41
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,10 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `animes_list`
+-- テーブルの構造 `animes_lists`
 --
 
-CREATE TABLE IF NOT EXISTS `animes_list` (
+CREATE TABLE IF NOT EXISTS `animes_lists` (
 `id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
@@ -48,6 +48,43 @@ CREATE TABLE IF NOT EXISTS `genres` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   `genre_name` varchar(64) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `genres`
+--
+
+INSERT INTO `genres` (`id`, `created`, `modified`, `genre_name`) VALUES
+(1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'SF'),
+(2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '日常系');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `reviews`
+--
+
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `animes_lists_id` int(11) NOT NULL,
+  `reviews` varchar(2000) NOT NULL,
+  `users_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `password` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -55,9 +92,9 @@ CREATE TABLE IF NOT EXISTS `genres` (
 --
 
 --
--- Indexes for table `animes_list`
+-- Indexes for table `animes_lists`
 --
-ALTER TABLE `animes_list`
+ALTER TABLE `animes_lists`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -67,18 +104,29 @@ ALTER TABLE `genres`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `animes_list`
+-- AUTO_INCREMENT for table `animes_lists`
 --
-ALTER TABLE `animes_list`
+ALTER TABLE `animes_lists`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
